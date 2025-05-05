69d691c80adcfbc9a3b377b2a06d59cfcd64619f
Kirill <karachevkir@yandex.ru>
2025-05-05 23:55:33 +0300

    add inline button in start message

--- keyboards.py
+++ keyboards.py
@@ -2,7 +2,14 @@ from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKe
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def get_ikb_what():
+def get_ikb_start():
     builder = InlineKeyboardBuilder()
-    builder.add(InlineKeyboardButton(text='Как этим пользоваться?🤔', callback_data='what'))
+    builder.add(InlineKeyboardButton(text='🎁 Оформить заказ ❤️', callback_data='make_order'),
+                InlineKeyboardButton(text='💰 Калькулятор стоимости', callback_data='what'),
+                InlineKeyboardButton(text='💬 Отзывы о нашей работе', url=''),
+                InlineKeyboardButton(text='📚 Ответы на популярные вопросы', callback_data='what'),
+                InlineKeyboardButton(text='📡 Отследить посылку', url=''),
+                InlineKeyboardButton(text='🧾 Задать вопрос', url=''),
+                InlineKeyboardButton(text='🏙️ Скачать POIZON iOS', url=''),
+                InlineKeyboardButton(text='📱 Скачать POIZON Android', url=''),)
     return builder.as_markup()
